f6a6d23aa4f0a3aafb06b4127172f981e88c86d6
vagrant <vagrant@homestead>
2015-11-21 10:34:45 +0000

    Initial Setup

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project Flyer</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+
+	<div class="container">
+
+		@yield('content')
+
+	</div>
+
+</body>
+</html>
